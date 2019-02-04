fc007702a152845a483de47186621215d5c1eefb
Sanjay Sai <shachsan@hotmail.com>
2019-02-04 13:35:40 -0500

    work on associatescontroller

--- app/controllers/api/v1/associates_controller.rb
+++ app/controllers/api/v1/associates_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::AssociatesController < ApplicationController
+    def index
+        @associates = Associate.where(department_id:params[:id])
+        rendor json: @associates
+    end 
+
+    def show
+        # byebug
+        @associates = Associate.where(department_id:associate_params[:id])
+        render json: @associates
+    end 
+
+    private
+    def associate_params
+        params.permit(:id, :name, :position, :department_id, :hours_per_week)
+    end
 end
